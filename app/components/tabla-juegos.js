118a4f5dab95590e60fba4d4182cab2ef4e74211
KevinBarrera <kev97barrera@gmail.com>
2019-05-04 19:59:55 -0500

    respaldo

--- app/components/tabla-juegos.js
+++ app/components/tabla-juegos.js
@@ -1,4 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class TablaJuegosComponent extends Component {
+    @service('user_selected') user_global;
+    @tracked games = this.args.games || [];
+    @tracked user_id="3";
 }
